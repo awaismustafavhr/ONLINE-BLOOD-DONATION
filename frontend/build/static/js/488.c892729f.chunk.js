dd4e15228ae30fb2d9b7778ca5759fc89f40fd8b
MUHAMMAD AWAIS MUSTAFA <awaismustafavhr@gmail.com>
2026-07-08 11:09:44 -0700

    Fix ESLint issues for production deployment

---
+++ frontend/build/static/js/488.c892729f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbloodlink_frontend=self.webpackChunkbloodlink_frontend||[]).push([[488],{488:(e,n,k)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.c892729f.chunk.js.map
